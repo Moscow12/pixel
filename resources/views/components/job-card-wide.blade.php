3ff8c5b534940d4ec7195b58d336ab2096ae87aa
Moscow12 <meshackstev@gmail.com>
2024-07-07 17:44:15 +0300

    fisrt init in new laravel model

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,16 @@
+<x-panel class="flex space-x-6">
+    <div>
+         <x-employee-logo :width="72"  />
+    </div>
+          
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-1000" >Vedio Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time From $60,000</p>
+    </div>
+    <div >
+        <x-tags>Tag</x-tags>
+        <x-tags>Tag</x-tags>
+        <x-tags>Tag</x-tags>       
+    </div>
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,19 @@
+{{-- <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-1000"> --}}
+<x-panel class="flex flex-col text-center"> 
+    <div class="self-start text-sm">
+        Laracast
+    </div>
+    <div class="py-8 ">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-1000">Vedio Producer</h3>
+        <p class="text-sm mt-4">Full Time From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto"> 
+        <div>
+           
+            <x-tags size="small" >Nurses</x-tags>
+            <x-tags size="small">Doctors</x-tags> 
+            <x-tags size="small">Pharmacists</x-tags>
+        </div> 
+        <x-employee-logo :width="29" />
+    </div>
+</x-panel>
