0bd0904fefac9a7ac26f1e5cbaafd4b551afab30
Moscow12 <meshackstev@gmail.com>
2024-08-07 10:12:07 +0300

    test

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <div>
+        <section class="text-center pt-6">
+            <h1 class="text-bold text-4xl">Lets Find your next Job</h1>
+            <form action="" class="mt-6">
+                <input type="text" class="rounded-xl bg-white/20 border-white/10 px-5 py-4 w-full max-w-xl" placeholder="Am looking for ....">
+            </form>
+        </section>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/reg', [RegisterUserController::class], 'create');
+Route::post('/register', [RegisterUserController::class], 'store');
+
+Route::get('/login', [SessionController::class], 'create');
+Route::post('/login', [SessionController::class], 'store');
+Route::delete('/logout', [SessionController::class], 'destroy');
